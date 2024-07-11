dbaf135261be1d8acd2f4f2c2aaef3aed5a789eb
glenndsilva1 <glenndsilva@rediffmail.com>
2024-07-11 14:15:36 +0530

    reduce.js

--- reduce.js
+++ reduce.js
@@ -5,3 +5,17 @@ const sum = numbers.reduce((accumulator, currentValue) => {
 }, 0);
 
 console.log(sum); // Output: 15
+
+const add = (x, v) => v ? v + x : x;
+const addA = v => add('a', v);
+const addB = v => add('b', v);
+const addC = v => add('c', v);
+const abc = addC(addB(addA()));
+console.log(abc); // "abc"
+// simple generic function composer …
+const compose = (...fns) => v => (
+  fns.reduce((acc, fn) => fn(acc), v)
+);
+const abc = compose(addA, addB, addC);
+console.log(abc()); // "abc"
+
