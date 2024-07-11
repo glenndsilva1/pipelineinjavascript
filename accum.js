542025c9dfed27ed7e8b255e36e99d9435a668df
glenndsilva1 <glenndsilva@rediffmail.com>
2024-07-11 13:53:39 +0530

    accum.js

--- accum.js
+++ accum.js
@@ -1,28 +1,10 @@
 // fns is array of functions x is a function output reduce accumulates all function output and lastly the function is called with initial value in x
 const pipe = (...fns) => (x) => fns.reduce((acc, f) => f(acc), x);
 
-const priceCalculator = (
-  taxPercentage = 0.3, 
-  serviceFees = 10, 
-  price, 
-  discount, 
-  percentCoupon, 
-  valueCoupon,
-  weight, 
-  $PerKg
-) => {
-  const applyTax           = (val) => val * (1 + taxPercentage)
-  const applyServiceFees   = (val) => val + serviceFees
-  const applyPercentCoupon = (val) => val - val * percentCoupon
-  const applyValueCoupon   = (val) => val - valueCoupon
-  const applyDiscount      = (val) => val - discount
-  const applyShippingCost  = (val) => val + weight * $PerKg
-return pipe(
-    applyPercentCoupon,
-    applyDiscount,
-    applyValueCoupon,
-    applyShippingCost,
-    applyServiceFees,
-    applyTax
-  )(price)
-}
+const addFive = (x) => x + 5;
+const square = (x) => x * x;
+const double = (x) => x * 2;
+
+const result = pipe(addFive, square, double)(2); // Returns 98
+
+console.log(result);
